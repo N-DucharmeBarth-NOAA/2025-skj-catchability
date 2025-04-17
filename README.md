f09c4de6d733bbf7ae49f280c5209044d280ebc1
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2025-04-17 14:49:37 +1100

    update output & README

--- README.md
+++ README.md
@@ -1,5 +1,83 @@
 # 2025-skj-catchability
-Code and data to calculate WCPO skipjack tuna catchability proxy for unassociated purse seine
+
+This repository contains an R script that calculates a free-school catchability proxy for skipjack tuna in the Western and Central Pacific Ocean (WCPO), following the methodology described in [Hamer et al. (2024)](https://meetings.wcpfc.int/node/22984).
+
+![Catchability Proxy Trends](output/q_proxy.png)
+
+## Overview
+
+The script `catchability_proxy.r` analyzes purse seine catch data and biomass estimates to calculate a catchability proxy, which is then used to assess potential effort creep in the WCPO purse seine fishery. Effort creep refers to the gradual increase in fishing efficiency over time due to technological improvements or changes in fishing practices.
+
+## Methodology
+
+The analysis follows these steps:
+
+1. Extract public purse seine catch data and restrict to assessment regions 6-8
+2. Process data for both unassociated (free-school) and drifting FAD fishing methods
+3. Extract biomass estimates from the 2022 skipjack diagnostic stock assessment model
+4. Calculate catchability proxy as catch/(sets × biomass)
+5. Normalize the proxy by setting the mean value to 1.0
+6. Perform linear regression to estimate annual percent change
+7. Generate visualizations of catchability trends
+
+## Findings
+
+The analysis reveals that the annual catchability proxy increases by approximately 2.5% for free-school using set as the effort metric. However, the proposed free-school index for the 2025 WCPO skipjack tuna stock assessment defines effort as distance traveled from a previous set. Previous work in support of the 2022 WCPO skipjack tuna stock assessment ([Teears et al., 2022](https://meetings.wcpfc.int/node/16253), Figure 14) did not indicate an appreciable change to the trend in the index when defining effort using distance traveled rather than set.
+
+
+## Data Sources
+
+- WCPFC public data repository: [Purse seine catch data](https://www.wcpfc.int/node/4648)
+- 2022 skipjack stock assessment ([diagnostic case T2G10.8](https://oceanfish.spc.int/en/ofpsection/sam/sam/213-skipjack-assessment-results#2022)) for biomass estimates
+
+## Requirements
+
+The script requires the following R packages:
+- data.table
+- magrittr
+- FLR4MFCL
+- ggplot2
+- this.path
+
+## Getting Started
+This project uses `renv` for package management to ensure reproducibility across environments. The repository is also configured to work with GitHub Codespaces for cloud-based development.
+
+### Running the Models Locally
+First, [clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository to your local machine.
+
+#### Using Base R
+1. Open an R terminal (recommended [version 4.4.0](https://cloud.r-project.org/) with RTools 4.4 installed)
+2. Set your working directory to the cloned repository:
+   ```r
+   setwd("path/to/repository/")
+   ```
+3. Source the `.Rprofile` file to bootstrap `renv`:
+   ```r
+   source(".Rprofile")
+   ```
+4. If `renv` doesn't bootstrap automatically, run:
+   ```r
+   renv::restore()
+   ```
+5. Follow the prompts to install all required packages (including `data.table`, `magrittr`, `ggplot2`, `FLR4MFCL`, and other dependencies)
+
+#### Using RStudio
+1. Open the repository as an [RStudio Project](https://bookdown.org/ndphillips/YaRrr/projects-in-rstudio.html)
+2. RStudio should automatically detect the `renv` configuration and prompt for package installation
+3. If `renv` doesn't bootstrap automatically, run:
+   ```r
+   renv::restore()
+   ```
+
+#### Using Visual Studio Code
+1. Configure VS Code to work with R using the [R extension](https://github.com/REditorSupport/vscode-R)
+2. Follow the specific [configuration steps for renv projects](https://github.com/REditorSupport/vscode-R/wiki/Working-with-renv-enabled-projects)
+3. Open the repository folder in VS Code
+4. Open an R terminal, which should prompt `renv` to bootstrap
+5. If `renv` doesn't bootstrap automatically, run:
+   ```r
+   renv::restore()
+   ```
 
 ## License
 
--- code/catchability_proxy.r
+++ code/catchability_proxy.r
@@ -25,7 +25,7 @@
 # Aggregated data, grouped by 5°x5° latitude/longitude grids, YEAR and MONTH.
 # PURSE SEINE fishery. Data cover 1950 to 2023 for the WCPFC Convention Area.
     catch_una_dt = fread(file.path(proj_dir,"data","WCPFC_S_PUBLIC_BY_YR_MON.CSV")) %>%
-      .[,.(days=sum(days),skj=sum(skj_c_una),total=sum(skj_c_una+yft_c_una+bet_c_una+oth_c_una)),by=.(yy,lat5,lon5)] %>%
+      .[,.(effort=sum(sets_una),skj=sum(skj_c_una),total=sum(skj_c_una+yft_c_una+bet_c_una+oth_c_una)),by=.(yy,lat5,lon5)] %>%
       .[,lat_num := as.numeric(gsub("N","",gsub("S","",lat5)))] %>%
       .[,lat_char := substr(lat5,3,3)] %>%
       .[,lat := ifelse(lat_char=="N",lat_num,-lat_num)+2.5] %>%
@@ -33,13 +33,13 @@
       .[,lon_char := substr(lon5,4,4)] %>%
       .[,lon := ifelse(lon_char=="E",lon_num,(180-lon_num)+180)+2.5] %>%
       .[lon>=140 & lon<=210 & lat>= -10 & lat <= 10] %>%
-      .[,.(days=sum(days),skj=sum(skj),total=sum(total)),by=.(yy)] %>%
-      melt(.,id.vars=c("yy","days")) %>%
+      .[,.(effort=sum(effort),skj=sum(skj),total=sum(total)),by=.(yy)] %>%
+      melt(.,id.vars=c("yy","effort")) %>%
       .[,set_type:="Un-Associated"] %>%
-      .[,.(set_type,yy,days,variable,value)]
+      .[,.(set_type,yy,effort,variable,value)]
     
     catch_dfad_dt = fread(file.path(proj_dir,"data","WCPFC_S_PUBLIC_BY_YR_MON.CSV")) %>%
-      .[,.(days=sum(days),skj=sum(skj_c_dfad),total=sum(skj_c_dfad+yft_c_dfad+bet_c_dfad+oth_c_dfad)),by=.(yy,lat5,lon5)] %>%
+      .[,.(effort=sum(sets_dfad),skj=sum(skj_c_dfad),total=sum(skj_c_dfad+yft_c_dfad+bet_c_dfad+oth_c_dfad)),by=.(yy,lat5,lon5)] %>%
       .[,lat_num := as.numeric(gsub("N","",gsub("S","",lat5)))] %>%
       .[,lat_char := substr(lat5,3,3)] %>%
       .[,lat := ifelse(lat_char=="N",lat_num,-lat_num)+2.5] %>%
@@ -47,10 +47,10 @@
       .[,lon_char := substr(lon5,4,4)] %>%
       .[,lon := ifelse(lon_char=="E",lon_num,(180-lon_num)+180)+2.5] %>%
       .[lon>=140 & lon<=210 & lat>= -10 & lat <= 10] %>%
-      .[,.(days=sum(days),skj=sum(skj),total=sum(total)),by=.(yy)] %>%
-      melt(.,id.vars=c("yy","days")) %>%
+      .[,.(effort=sum(effort),skj=sum(skj),total=sum(total)),by=.(yy)] %>%
+      melt(.,id.vars=c("yy","effort")) %>%
       .[,set_type:="Drifting FAD"] %>%
-      .[,.(set_type,yy,days,variable,value)]
+      .[,.(set_type,yy,effort,variable,value)]
     
     catch_dt = rbind(catch_una_dt,catch_dfad_dt)
 
@@ -67,7 +67,7 @@
 
     # merge data
     q_dt = merge(catch_dt,bio_dt,by="yy") %>%
-        .[,q_proxy:=value/days/bio] %>%
+        .[,q_proxy:=value/effort/bio] %>%
         .[yy>=2008] %>%
         .[,q_proxy:=q_proxy/mean(q_proxy),by=variable] %>%
         .[,variable:=factor(variable,levels=c("skj","total"),labels=c("Skipjack catch","Total catch"))] %>%
@@ -76,16 +76,40 @@
     fwrite(q_dt,file=file.path("output","q_dt.csv"))
 
 #_____________________________________________________________________________________________________________________________
+# Calculate linear regression of q_proxy by catch_type and set_type
+    # Fit linear models and store the results
+    lm_results = q_dt[, .(
+        intercept = coef(lm(q_proxy ~ yy))[1],
+        slope = coef(lm(q_proxy ~ yy))[2],
+        pvalue = summary(lm(q_proxy ~ yy))$coefficients[2,4],
+        r_squared = summary(lm(q_proxy ~ yy))$r.squared,
+        annual_percent_change = coef(lm(q_proxy ~ yy))[2] * 100  # Convert to percentage
+    ), by = .(catch_type, set_type)]
+    
+    # Save to a CSV file
+    fwrite(lm_results, file = file.path("output", "q_proxy_trends.csv"))
+
+#_____________________________________________________________________________________________________________________________
 # make figures
     p = ggplot(q_dt,aes(x=yy,y=q_proxy)) +
-        facet_grid(set_type~catch_type) +
+        facet_grid(set_type~catch_type,scales="free_y") +
         geom_point() +
         geom_smooth(method = "lm") +
         ylab("Catchability proxy (mean-centered)") +
         xlab("Year") +
+        geom_text(
+            data = lm_results,
+            aes(x = Inf, y = -Inf, 
+                label = sprintf("\nAnnual change: %.2f%%\n", 
+                               annual_percent_change)),
+            hjust = 1, vjust = 0,
+            size = 3,
+            position = position_nudge(y = 0.1)) +
         theme(panel.background = element_rect(fill = "white", color = "black", linetype = "solid"),
                     panel.grid.major = element_line(color = 'gray70', linetype = "dotted"),
                     panel.grid.minor = element_line(color = 'gray70', linetype = "dotted"),
                     strip.background = element_rect(fill = "white"),
                     legend.key = element_rect(fill = "white"))
-
+    
+    ggsave(filename = file.path("output","q_proxy.png"), plot = p, width = 6, height = 6, 
+              dpi = 300, units = "in")
